344df6083472ad17fe8718d9b74f2491b3374899
Nakorn Sinpadung <nakorndev@gmail.com>
2025-10-08 21:18:38 +0700

    update crud with swagger for tickets

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- backend/src/tickets/tickets.controller.ts
+++ backend/src/tickets/tickets.controller.ts
@@ -7,38 +7,241 @@ import {
   Param,
   Delete,
   Query,
+  NotFoundException,
 } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
 import { QueryTicketDto } from './dto/query-ticket.dto';
+import { ApiResponse } from '@nestjs/swagger';
+
+const apiResponseTicketData = {
+  id: { type: 'string', example: 'cuid' },
+  title: { type: 'string', example: 'My first ticket' },
+  description: {
+    type: 'string',
+    example: 'This is my first ticket',
+  },
+  priority: { type: 'string', example: 'LOW' },
+  status: { type: 'string', example: 'OPEN' },
+  createdAt: {
+    type: 'string',
+    example: '2023-01-01T00:00:00.000Z',
+  },
+  updatedAt: {
+    type: 'string',
+    example: '2023-01-01T00:00:00.000Z',
+  },
+};
 
 @Controller('tickets')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
   @Post()
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketsService.create(createTicketDto);
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 201,
+        },
+        message: {
+          type: 'string',
+          example: 'The record has been successfully created.',
+        },
+        data: {
+          type: 'object',
+          properties: apiResponseTicketData,
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 400,
+        },
+        message: {
+          type: 'array',
+          items: {
+            type: 'string',
+            example: 'Ticket title is required',
+          },
+        },
+        error: {
+          type: 'string',
+          example: 'Bad Request',
+        },
+      },
+    },
+  })
+  async create(@Body() createTicketDto: CreateTicketDto) {
+    const { id } = await this.ticketsService.create(createTicketDto);
+    return { id, message: 'The record has been successfully created.' };
   }
 
   @Get()
-  findAll(@Query() query: QueryTicketDto) {
-    return this.ticketsService.findAll(query);
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    schema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: apiResponseTicketData,
+          },
+        },
+        total: { type: 'number', example: 1 },
+        totalPages: { type: 'number', example: 1 },
+        page: { type: 'number', example: 1 },
+        pageSize: { type: 'number', example: 10 },
+        statusCode: { type: 'number', example: 200 },
+        message: { type: 'string', example: 'Fetch all tickets' },
+      },
+    },
+  })
+  async findAll(@Query() query: QueryTicketDto) {
+    const tickets = await this.ticketsService.findAll(query);
+    return {
+      ...tickets,
+      message: 'Fetch all tickets',
+      statusCode: 200,
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ticketsService.findOne(id);
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'number', example: 200 },
+        message: { type: 'string', example: 'Ticket found' },
+        data: {
+          type: 'object',
+          properties: apiResponseTicketData,
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 404,
+        },
+        message: {
+          type: 'string',
+          example: 'Ticket not found',
+        },
+        error: {
+          type: 'string',
+          example: 'Not Found',
+        },
+      },
+    },
+  })
+  async findOne(@Param('id') id: string) {
+    const ticket = await this.ticketsService.findOne(id);
+    if (!ticket) {
+      throw new NotFoundException('Ticket not found');
+    }
+    return {
+      statusCode: 200,
+      message: 'Ticket found',
+      data: ticket,
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
-    return this.ticketsService.update(id, updateTicketDto);
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'number', example: 200 },
+        message: {
+          type: 'string',
+          example: 'The record has been successfully updated.',
+        },
+        data: {
+          type: 'object',
+          properties: apiResponseTicketData,
+        },
+      },
+    },
+  })
+  async update(
+    @Param('id') id: string,
+    @Body() updateTicketDto: UpdateTicketDto,
+  ) {
+    await this.ticketsService.update(id, updateTicketDto);
+    return {
+      statusCode: 200,
+      message: 'The record has been successfully updated.',
+      data: await this.ticketsService.findOne(id),
+    };
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ticketsService.remove(id);
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: { type: 'number', example: 200 },
+        message: {
+          type: 'string',
+          example: 'The record has been successfully deleted.',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 404,
+        },
+        message: {
+          type: 'string',
+          example: 'Ticket not found',
+        },
+        error: {
+          type: 'string',
+          example: 'Not Found',
+        },
+      },
+    },
+  })
+  async remove(@Param('id') id: string) {
+    await this.ticketsService.remove(id);
+    return {
+      statusCode: 200,
+      message: 'The record has been successfully deleted.',
+    };
   }
 }
